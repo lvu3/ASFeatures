bf3b867f70577def02d58fdef2f918fd7080db88
lvu3 <107505679+lvu3@users.noreply.github.com>
2023-03-07 11:47:44 -0500

    Delete Contact button directory

--- Contact button/luContactBtn.js
+++
@@ -1,8 +0,0 @@
-function openBtn() {
-    const elem = document.getElementById('contactUsContent');
-    elem.style.display = 'flex';
-    }
-    function closeBtn() {
-    const elem = document.getElementById('contactUsContent');
-    elem.style.display = 'none';
-    } 
